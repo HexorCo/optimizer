142592a679f3748848f30fc7682f6cf50d834f6e
tirl_dev <97565183+tirldev@users.noreply.github.com>
2023-07-15 18:13:57 -0400

    Add files via upload

---
+++ optimizer_updater/optimizer_updater/optimizer_updater.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace optimizer_updater
+{
+    partial class optimizer_updater
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(optimizer_updater));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(85, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Downloading...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(159)))), ((int)(((byte)(155)))));
+            this.label2.Location = new System.Drawing.Point(71, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exceeding expectations";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(159)))), ((int)(((byte)(155)))));
+            this.label3.Location = new System.Drawing.Point(72, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "always reinventing myself";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(104, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 40);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Optimizer";
+            // 
+            // optimizer_updater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.ClientSize = new System.Drawing.Size(374, 396);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "optimizer_updater";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Optimizer_Updater";
+            this.Load += new System.EventHandler(this.optimizer_updater_LoadAsync);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ optimizer_updater/optimizer_updater/optimizer_updater.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Security.Policy;
+
+namespace optimizer_updater
+{
+    public partial class optimizer_updater : Form
+    {
+        public optimizer_updater()
+        {
+            InitializeComponent();
+        }
+
+        private void optimizer_updater_LoadAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var process in Process.GetProcessesByName("optimizer"))
+                {
+                    process.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+          
+
+
+
+            string url_asar = "";
+
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    string response = webClient.DownloadString("https://raw.githubusercontent.com/tirldev/optimizer/main/update/l_asar.optimizer");
+                    url_asar = response.Replace("\n", ""); ;
+                }
+                catch (WebException ex)
+                {
+                }
+            }
+
+            using (WebClient webClient2 = new WebClient())
+            {
+                try
+                {
+                    webClient2.DownloadFileCompleted += DescargaCompletada;
+                    webClient2.DownloadFileAsync(new Uri(url_asar), Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\programs\optimizer\resources\app.asar");
+                }
+                catch (WebException ex)
+                { 
+                }
+            }
+        }
+        static void DescargaCompletada(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                try
+                {
+                    Process.Start("start "+ Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Programs\optimizer\optimizer.exe");
+                }
+                catch (Exception ex)
+                {
+
+                }
+                Thread.Sleep(1000);
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
